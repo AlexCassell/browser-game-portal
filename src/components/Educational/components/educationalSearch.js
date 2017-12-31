6be6a9673fafa0da972d0fc26f22456d05427a09
Alex Cassell <alex@simulism.net>
2017-12-31 00:32:50 -0500

    Keyword and Search started

--- src/components/Educational/components/educationalSearch.js
+++ src/components/Educational/components/educationalSearch.js
@@ -6,29 +6,54 @@ import educationalGamesArray from '../components/educationalGamesArray';
 
 import '../css/styles.css';
 
-let searchInput = "", keywordArray = [], keywordCount = 0, tempLetterHolder ="";
+let searchInput = "", keywordArray = [], keywordCount = 0, tempLetterHolder ="", searchText=<div className="search__keywords">{searchInput}<span className="keywordArray">{keywordArray}</span></div>;
 
 class EducationalSearch extends Component {
     constructor(props) {
         super(props);
-        this.state = { 'name': ''}; 
+        this.state = { 'name': ''};
     }
 
 
+
     handleInput(e) {
-        if(e.key === 'Enter'){
-            console.log('enter press here! ');
+        this.immediateSearch(e);
+        this.keywords(e);
         }
+    
+    immediateSearch(e){
+        for(let s = 0; s < educationalGamesArray.length; s++){
+            for(let se = 0; se < educationalGamesArray[s].length; se++)
+            if(educationalGamesArray[s][se].includes(e.target.value)){
+                console.log("Name of Game: " + educationalGamesArray[s][0]);
+            }
+        }
+    }
+
+    keywords(e){
         this.setState({
             name: e.target.value
         });
         tempLetterHolder = e.target.value;
 
         if(tempLetterHolder.length > 9){
-            tempLetterHolder = tempLetterHolder.slice(0, -1)
-            console.log(e.target.value);
-            e.target.value = tempLetterHolder;
-            console.log(e.target.value);
+            tempLetterHolder = tempLetterHolder.replace(/[^a-z]/gi, '');
+            if(tempLetterHolder !== ""){
+                if(keywordCount === 6){
+                    keywordArray.splice(0, 1);
+                    keywordArray[0] = keywordArray[0].replace(/[^a-z]/gi, '');
+                }
+                else{
+                    keywordCount++;
+                }
+                if(keywordCount === 1){
+                    keywordArray.push(tempLetterHolder);
+                }
+                else{
+                    keywordArray.push(", " + tempLetterHolder);
+                }
+                e.target.value = "";
+            }
             this.setState({
                 name: e.target.value
             });
@@ -46,8 +71,21 @@ class EducationalSearch extends Component {
                 if(tempLetterHolder !== " "){
                     tempLetterHolder = tempLetterHolder.replace(/[^a-z]/gi, '');
                     if(tempLetterHolder !== ""){
-                        keywordArray.push(" " + tempLetterHolder);
-                        keywordCount++;
+                        if(keywordCount === 6){
+                            keywordArray.splice(0, 1);
+                            keywordArray[0] = keywordArray[0].replace(/[^a-z]/gi, '');
+                        }
+                        else{
+                            keywordCount++;
+                        }
+                        if(keywordCount === 1){
+                            keywordArray.push(tempLetterHolder);
+                        }
+                        else{
+                            keywordArray.push(", " + tempLetterHolder);
+                        }
+                        
+                        
                     }
                     e.target.value = "";
                     this.setState({
@@ -55,10 +93,18 @@ class EducationalSearch extends Component {
                     });
                 }
             }
-                //everytime  space is pressed
             }
-            searchInput = "Searching for games with the keyword(s): " + keywordArray;
-        }
+            if(keywordCount > 1){
+                searchInput = "Searching for games with the keywords: ";
+            }
+            else if(keywordCount === 1) {
+                searchInput = "Searching for games with the keyword: ";
+            }
+            else if(keywordCount === 0) {
+                searchInput = "";
+            }
+    }
+
 
     
 
@@ -66,7 +112,7 @@ class EducationalSearch extends Component {
         return (
             <div>
                 <div className="search"><input type="text" className="search__form" value={this.state.name} onChange={ (e) => this.handleInput(e) } /></div>
-                {searchInput}
+                <div className="search__keywords">{searchInput}<span className="keywordArray">{keywordArray}</span></div>
             </div>
             );
         }
